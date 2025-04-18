ef0accc911895ede8d025ca556506ceb28b5cb1b
TylerYemeni <leomessi111909@gmail.com>
2025-04-18 22:44:51 +0300

    Update RealisticReportEngine.kt

--- RealisticReportEngine.kt
+++ RealisticReportEngine.kt
@@ -5,19 +5,9 @@ import kotlin.random.Random
 
 object RealisticReportEngine {
 
-    fun performReport(accountId: String, targetNumber: String): Boolean {
-        try {
-            // تأخير عشوائي ذكي لمحاكاة التفاعل البشري
-            val delay = Random.nextInt(2000, 6000)
-            Thread.sleep(delay.toLong())
-
-            // هنا يمكن ربط API أو تنفيذ بلاغ حقيقي
-            Log.d("RealisticReportEngine", "[$accountId] بلاغ وهمي على $targetNumber بعد $delay مللي ثانية")
-
-            return true // اعتبرنا أنه تم الإبلاغ بنجاح
-        } catch (e: Exception) {
-            Log.e("RealisticReportEngine", "خطأ أثناء تنفيذ البلاغ: ${e.message}")
-            return false
-        }
+    fun simulateReport(accountId: String, target: String) {
+        val delay = Random.nextInt(3000, 7000)
+        Thread.sleep(delay.toLong())
+        Log.d("RealisticEngine", "[$accountId] بلاغ واقعي تم على $target بعد تأخير $delay مللي")
     }
 }
