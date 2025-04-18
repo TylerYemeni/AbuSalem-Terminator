a7c07eb19bb3fa03b54aec4774b595a462233aa2
TylerYemeni <leomessi111909@gmail.com>
2025-04-18 18:35:06 +0300

    Update NumberGenerator.kt

--- NumberGenerator.kt
+++ NumberGenerator.kt
@@ -1,14 +1,30 @@
-package com.abusalem.generator
-
-import java.util.*
+package com.abusalem.guardian.utils
 
 object NumberGenerator {
-    private val countryCodes = listOf("+966", "+20", "+1", "+971", "+218", "+44")
+    private val arabCountries = mapOf(
+        "+967" to "اليمن",
+        "+966" to "السعودية",
+        "+20"  to "مصر",
+        "+963" to "سوريا",
+        "+964" to "العراق",
+        "+218" to "ليبيا",
+        "+212" to "المغرب",
+        "+971" to "الإمارات",
+        "+973" to "البحرين",
+        "+962" to "الأردن",
+        "+213" to "الجزائر",
+        "+216" to "تونس"
+    )
 
-    fun generateRandomNumber(): String {
-        val random = Random()
-        val code = countryCodes[random.nextInt(countryCodes.size)]
+    fun generateRandomArabicNumber(): String {
+        val (code, _) = arabCountries.entries.random()
         val number = (100000000..999999999).random()
         return "$code$number"
+    }
+
+    fun generateNumberByCode(code: String): String {
+        val validCode = if (arabCountries.containsKey(code)) code else "+967"
+        val number = (100000000..999999999).random()
+        return "$validCode$number"
     }
 }
